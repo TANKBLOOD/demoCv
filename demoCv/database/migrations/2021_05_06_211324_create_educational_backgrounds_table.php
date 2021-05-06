f98931cce33767a626d07871438b54933025f8d7
TANK <gholizadeh.pro@gmail.com>
2021-05-07 01:48:13 +0430

    educational background model created.

---
+++ demoCv/database/migrations/2021_05_06_211324_create_educational_backgrounds_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEducationalBackgroundsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('educational_backgrounds', function (Blueprint $table) {
+            $table->id();
+            $table->string('degree_name');
+            $table->string('institution_name');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->text('additional_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('educational_backgrounds');
+    }
+}
