486b52b29a15357ce15ec9ccc907cf806368e159
TANK <gholizadeh.pro@gmail.com>
2021-05-07 01:42:28 +0430

    workExperiance model created

---
+++ demoCv/database/migrations/2021_05_06_211002_create_work_experiences_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('work_place_name');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->text('additional_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_experiences');
+    }
+}
