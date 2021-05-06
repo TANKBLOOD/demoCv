6e0167b524ef5d409aa3df9d5bfd3cac9d96f6ca
TANK <gholizadeh.pro@gmail.com>
2021-05-07 01:38:08 +0430

    personalInfo model created

---
+++ demoCv/database/migrations/2021_05_06_210301_create_personal_infos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cv_id');
+            $table->string('image_path');
+            $table->string('f_name');
+            $table->string('current_job');
+            $table->string('city');
+            $table->string('gender');
+            $table->string('phone_number');
+            $table->string('email');
+            $table->string("birth_date");
+            $table->string('marital_status');
+            $table->text('about_me');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_infos');
+    }
+}
