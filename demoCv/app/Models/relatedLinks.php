cd726e138fb3c112cfd3137b86b0827915d0d4a1
TANK <gholizadeh.pro@gmail.com>
2021-05-07 17:30:59 +0430

    named changed

--- demoCv/app/Models/relatedLinks.php
+++ demoCv/app/Models/relatedLinks.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class relatedLinks extends Model
+class RelatedLinks extends Model
 {
+    protected $table= 'related_links';
     use HasFactory;
     public function cv() {
         return $this->belongsTo(LightCv::class, 'cv_id');
